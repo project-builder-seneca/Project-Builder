3b9a73b2f28682e3b96b096cd87134e1bb1a61a2
Eh <heklove@paran.com>
2019-06-12 17:49:00 -0400

    Editing Register

---
+++ Project Builder Development/Models/Extended/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_Builder_Development.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name required")]
+        [StringLength(25, ErrorMessage = "First Name must be {2} to {0} characters long.", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(25, ErrorMessage = "Last Name must be atleast {2} characters long.", MinimumLength = 2)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email address required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailID { get; set; }
+
+        [Display(Name = "Project Builder ID")]
+        [StringLength(25, ErrorMessage = "UserId must be {2} to {0} characters long.", MinimumLength = 3)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Project Builder ID required")]
+        public string PBID { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(25, ErrorMessage = "Password must be {2} to {0} characters long.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
---
+++ Project Builder Development/Models/ResetPasswordModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_Builder_Development.Models
+{
+    public class ResetPasswordModel
+    {
+        [StringLength(25, ErrorMessage = "Password must be {2} to {0} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "New Password required", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "New password and confirm password does not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string ResetCode { get; set; }
+    }
+}
