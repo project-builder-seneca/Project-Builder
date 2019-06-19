3a7f4657caccd9f440f22bf7373df850452df361
SangJun-HitHub <sjpark24@myseneca.ca>
2019-06-19 00:24:15 -0400

    sangjun

--- Project Builder Development/Models/IdeaBaseViewModel.cs
+++ Project Builder Development/Models/IdeaBaseViewModel.cs
@@ -36,14 +36,14 @@ namespace Project_Builder_Development.Models
         [Display(Name = "Investment Goal")]
         public int InvestmentGoal { get; set; }
 
-        [Display(Name = "Partners Required: ")]
+        [Display(Name = "Partners Required")]
         public int PartnersRequired { get; set; }
 
         [Display(Name = "Skills Required for Partners: ")]
         [StringLength(25, ErrorMessage = "Please Choose from the provided list!", MinimumLength = 2)]
         public IEnumerable<SkillBaseViewModel> PatSkills { get; set; }
 
-        [Display(Name = "Volunteers Required: ")]
+        [Display(Name = "Volunteers Required")]
         public int VolunteersRequired { get; set; }
 
         [Display(Name = "Skills required for Volunteers: ")]
